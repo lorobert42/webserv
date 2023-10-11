bf7f24fdb2d591402e9fc4f3700b71ecc71bf7b2
MrMobbi <julliatmathias06@gmail.com>
2023-10-11 16:37:25 +0200

    Client class created

--- srcs/client/Client.hpp
+++ srcs/client/Client.hpp
@@ -6,29 +6,29 @@
 /*   By: lorobert <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 16:44:56 by lorobert          #+#    #+#             */
-/*   Updated: 2023/10/10 16:49:52 by lorobert         ###   ########.fr       */
+/*   Updated: 2023/10/11 14:39:18 by mjulliat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Server.hpp"
+#pragma once
+
+#include "../server/Server.hpp"
+#include <iostream>
 #include <string>
 
 class Client
 {
 	public:
-		Server&			getServer() const;
-		int				getSocket() const;
-		std::string		getRequest() const;
-
+		Client(void);
 		Client(Server const& server, int client_socket);
 		Client(Client const& other);
 		Client& operator=(Client const& other);
-		~Client();
+		~Client(void);
 	
 	private:
-		Server 			_server;
+		Server			_server;
 		int				_socket;
 		std::string		_request;
 
-		Client();
+
 };
