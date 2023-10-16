f1bb8d3b2454c1e2f4eb5eebe41dc5fb8c62d8a4
MrMobbi <julliatmathias06@gmail.com>
2023-10-16 12:15:06 +0200

    Write and read handler in client class

--- srcs/client/Client.hpp
+++ srcs/client/Client.hpp
@@ -6,7 +6,11 @@
 /*   By: lorobert <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 16:44:56 by lorobert          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2023/10/16 14:55:53 by lorobert         ###   ########.fr       */
+=======
+/*   Updated: 2023/10/16 12:14:22 by mjulliat         ###   ########.fr       */
+>>>>>>> a3a49b2 (Write and read handler in client class)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +19,25 @@
 #include "../server/Server.hpp"
 #include <iostream>
 #include <string>
+#include <fstream>
 
 class Client
 {
 	public:
 		Client(void);
-		Client(Server const& server, int client_socket);
+		Client(Server *server, int &client_socket);
 		Client(Client const& other);
 		Client& operator=(Client const& other);
 		~Client(void);
+
+		int	getSocket(void) const;
+		std::string getRequest(void) const;
+
+		int	readHandler(void);
+		int writeHandler(void);
 	
 	private:
-		Server			_server;
+		Server			*_server;
 		int				_socket;
 		std::string		_request;
 
