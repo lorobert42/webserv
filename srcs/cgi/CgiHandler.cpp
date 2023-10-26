bca5d847cb68adc2d83328ba8586125dbbc35345
Sylvain Nicolet <nicoletsylvain@gmail.com>
2023-10-26 09:29:41 +0200

    Small refactoring

--- srcs/cgi/CgiHandler.cpp
+++ srcs/cgi/CgiHandler.cpp
@@ -8,8 +8,6 @@ CgiHandler::CgiHandler(Client *client) {
 	this->_env["SERVER_SOFTWARE"] = "Webserv/1.0";
 	this->_env["SERVER_NAME"] = "TODO";
 	this->_env["GATEWAY_INTERFACE"] = "CGI/1.1";
-	this->_env["REDIRECT_STATUS"] = "200";
-	this->_env["SCRIPT_FILENAME"] = "/home/webserv/www/cgi/env.cgi.php";
 
 	// TODO: Set environment variables dedicated for the request
 	this->_env["SERVER_PROTOCOL"] = client->getRequest()->getVersion();
@@ -37,6 +35,12 @@ CgiHandler::CgiHandler(Client *client) {
 	this->_env["HTTP_REFERER"] = "TODO"; // client->getRequest()->getValue("Referer");
 	this->_env["HTTP_USER_AGENT"] = client->getRequest()->getValue("User-Agent");
 	this->_env["HTTP_COOKIE"] = "TODO";
+
+	// Set environment variables dedicated for PHP
+	this->_env["REDIRECT_STATUS"] = "200";
+	this->_env["SCRIPT_FILENAME"] = "www/cgi/env.cgi.php"; // TODO: Get the path from the config
+	// TODO: Check extension of script filename and get the right CGI
+	this->_env["SCRIPT_CGI"] = "/usr/bin/php-cgi";
 }
 
 CgiHandler::CgiHandler(CgiHandler const &src) {
@@ -79,11 +83,11 @@ char	**CgiHandler::_getEnv() {
 
 std::string CgiHandler::executeCgi() {
 	pid_t pid;
-	int pipefd[2];  // File descriptor array for the pipe
+	int fd[2];  // File descriptor array for the pipe
 	char** env = this->_getEnv();
 	std::string body;
 
-	if (pipe(pipefd) == -1) {
+	if (pipe(fd) == -1) {
 		throw std::runtime_error("[CGI] pipe() failed");
 	}
 
@@ -95,17 +99,16 @@ std::string CgiHandler::executeCgi() {
 	// This code will be executed by the child process
 	else if (pid == 0) {
 		// Close the read end of the pipe
-		close(pipefd[0]);
+		close(fd[0]);
 
 		// Redirect stdout to the write end of the pipe
-		dup2(pipefd[1], STDOUT_FILENO);
-		close(pipefd[1]);
-
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[1]);
 		// Prepare the argument list
-		char* argv[3] = {const_cast<char*>("/usr/bin/php-cgi"), const_cast<char*>("/home/webserv/www/cgi/env.cgi.php"), NULL};
+		char* argv[3] = {const_cast<char*>(this->_env["SCRIPT_CGI"].c_str()), const_cast<char*>(this->_env["SCRIPT_FILENAME"].c_str()), NULL};
 
 		// Execute the CGI script
-		if (execve("/usr/bin/php-cgi", argv, env) == -1) {
+		if (execve(this->_env["SCRIPT_CGI"].c_str(), argv, env) == -1) {
 			perror("[CGI] execve() failed");
 			_exit(EXIT_FAILURE);
 		}
@@ -113,12 +116,12 @@ std::string CgiHandler::executeCgi() {
 	// This code will be executed by the parent process
 	else {
 		// Close the write end of the pipe
-		close(pipefd[1]);
+		close(fd[1]);
 
 		// Read the output from the pipe into the 'body' variable
 		char buffer[4096];
 		ssize_t bytesRead;
-		while ((bytesRead = read(pipefd[0], buffer, sizeof(buffer))) > 0) {
+		while ((bytesRead = read(fd[0], buffer, sizeof(buffer))) > 0) {
 			body.append(buffer, bytesRead);
 		}
 
@@ -127,18 +130,13 @@ std::string CgiHandler::executeCgi() {
 		if (waitpid(pid, &status, 0) == -1) {
 			perror("[CGI] waitpid() failed");
 		} else {
-			if (WIFEXITED(status)) {
-				int exitStatus = WEXITSTATUS(status);
-				// You can process the exit status here if needed
-				(void)exitStatus;
-			} else if (WIFSIGNALED(status)) {
-				int terminatingSignal = WTERMSIG(status);
-				// You can handle the signal termination here if needed
-				(void)terminatingSignal;
-			}
+			if (status == 2)
+				return ("508 Loop Detected\r\n");
+			if (status != 0) {
+				return ("502 Bad Gateway\r\n");
 		}
 		// Close the read end of the pipe
-		close(pipefd[0]);
+		close(fd[0]);
 	}
 
 	// Free env
@@ -146,8 +144,6 @@ std::string CgiHandler::executeCgi() {
 		delete[] env[i];
 	}
 	delete[] env;
-
-	std::cout << "Body: " << body << std::endl;
 
 	return body;
 }
