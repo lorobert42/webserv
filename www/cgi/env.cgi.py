51c0b43618f84aefaa30a0c076b8b78124261c8b
Sylvain Nicolet <nicoletsylvain@gmail.com>
2023-11-13 15:45:32 +0100

    Remove env.cgi.py

--- www/cgi/env.cgi.py
+++
@@ -1,8 +0,0 @@
-import os
-
-print('Content-Type: text/html')
-print('')
-print('<h1>Environment Variables</h1>')
-env = os.environ
-for k in env.keys():
-    print('<b>%s</b>: %s<br>' % (k, env[k]))
